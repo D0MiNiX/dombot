a0e9301f2dfe6e549d7535fe182f84df36af4d0e
Meet Patel <geek96@live.com>
2022-04-24 21:59:04 +0530

    fix path issue while backing up bot

--- backup_job.py
+++ backup_job.py
@@ -10,9 +10,11 @@ DOMBOT_BACKUP_CHANNEL = -1001463171286
 
 async def create_and_send_backup():
     current_time = arrow.now().format("DD_MM_YYYY-HH_mm_ss")
-    zip_file_name = f"/home/ec2-user/dombot_backup_{current_time}"
-    zip_file_path = shutil.make_archive(zip_file_name, "zip", "/home/ec2-user/dombot")
+    home_path = os.path.expanduser("~")
+    zip_file_name = f"{home_path}/dombot_backup_{current_time}"
+    zip_file_path = shutil.make_archive(zip_file_name, "zip", f"{home_path}/dombot")
     await vars.bot.send_file(DOMBOT_BACKUP_CHANNEL, file=f"{zip_file_path}")
+    
     if os.path.exists(zip_file_path):
        os.remove(zip_file_path)
 
