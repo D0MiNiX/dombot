210d9265e2d59dbb4c6c84e5fbc0b6862f47f5c3
Dominic <geek96@live.com>
2023-05-04 11:26:45 +0530

    enable ubot

--- dombot_main.py
+++ dombot_main.py
@@ -61,8 +61,8 @@ def main():
     # bot.add_event_handler(blek_magic.set_current_level)
 
     # dom user bot
-    #dom.add_event_handler(blek_magic.cw)
-    #dom.add_event_handler(blek_magic.bot_testing)
+    dom.add_event_handler(blek_magic.cw)
+    dom.add_event_handler(blek_magic.bot_testing)
     dom.add_event_handler(report_leaders.cw_report_channel)
 
     # create backup jobs
